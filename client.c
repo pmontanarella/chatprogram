7d458eb2c2de5a240aa7f50d988b155a72926fa2
SarahLootah <sar-ah-loo-tah@hotmail.com>
2014-07-04 08:33:42 -0400

    client.c

--- client.c
+++ client.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <string.h>
 
 void error(char *msg)
 {
