b51d2c4470aa70cb410fab06fc83ee5d91d8c057
SarahLootah <sar-ah-loo-tah@hotmail.com>
2014-07-11 14:44:28 -0400

    edited structure

--- ourserver.c
+++ ourserver.c
@@ -8,59 +8,60 @@
 
 void error(char *msg)
 {
-    perror(msg);
-    exit(1);
+	perror(msg);
+	exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen;
-     char buffer[256];
-     char reply[256];
-     struct sockaddr_in serv_addr, cli_addr;
-     int n;
+	int sockfd, newsockfd, portno, clilen;
+	char buffer[256];
+	char reply[256];
+	struct sockaddr_in serv_addr, cli_addr;
+	int n;
 
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
+	if (argc < 2) {
+		fprintf(stderr,"ERROR, no port provided\n");
+         	exit(1);
+     	}
 
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-     if (sockfd < 0) 
-        error("ERROR opening socket");
+     	if (sockfd < 0) 
+        	error("ERROR opening socket");
 
-     bzero((char *) &serv_addr, sizeof(serv_addr));
+     	bzero((char *) &serv_addr, sizeof(serv_addr));
 
-     portno = atoi(argv[1]);
+     	portno = atoi(argv[1]);
 
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+     	serv_addr.sin_family = AF_INET;
+     	serv_addr.sin_addr.s_addr = INADDR_ANY;
+     	serv_addr.sin_port = htons(portno);
      
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
+     	if (bind(sockfd, (struct sockaddr *) &serv_addr,
+              	sizeof(serv_addr)) < 0) 
+              	error("ERROR on binding");
 
-     while(1){
-     listen(sockfd,5);
-     clilen = sizeof(cli_addr);
-     newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     	while(1){
+     		listen(sockfd,5);
+     		clilen = sizeof(cli_addr);
+     		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, 
+				   &clilen);
    
-     if (newsockfd < 0) 
-          error("ERROR on accept");
+     	if (newsockfd < 0) 
+          	error("ERROR on accept");
+     	bzero(buffer,256);
+     	n = read(newsockfd,buffer,255);
      
-     bzero(buffer,256);
-     
-     n = read(newsockfd,buffer,255);
-     
-     if (n < 0) error("ERROR reading from socket");
-     printf("Here is the message: %s\n",buffer);
-     printf("reply:");
-     fgets(reply,255,stdin);
+     	if (n < 0) error("ERROR reading from socket");
+     	printf("Here is the message: %s\n",buffer);
+     	printf("reply:");
+     	fgets(reply,255,stdin);
 	n = write(newsockfd,reply,255);
-     if (n < 0) error("ERROR writing to socket");
+     	
+	if (n < 0) error("ERROR writing to socket");
 	}
-     return 0; 
+     	
+	return 0; 
 }
